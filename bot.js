25578ac846b4a43b824abf52bcdf79fec189659d
Mehdi7894 <47418152+Mehdi7894@users.noreply.github.com>
2019-02-26 13:44:35 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,9 +4,9 @@ console.log("Scrpit By Dream");
 
 
 client.on("ready", () => {
-let channel =     client.channels.get("هنا اي دي الروم")
+let channel =     client.channels.get("549933416687337483")
 setInterval(function() {
-channel.send(`هنا كلام ال سبام`);
+channel.send(`استغر الله عضيم واتوب اليه`);
 }, 30)
 })
 
